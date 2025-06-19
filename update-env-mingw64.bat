f8524b1c3419dbdbf348eea62a4ecfa69ede62b2
Benilda Key <BKey76@GMail.com>
2025-06-19 00:49:55 -0500

    Update environment.

--- update-env-mingw64.bat
+++ update-env-mingw64.bat
@@ -2,6 +2,9 @@
 set _ShowDebugMessages=%~1
 if "%_ShowDebugMessages%" equ "" set _ShowDebugMessages=no
 
+set _IncludeBusyBox=%~2
+if "%_IncludeBusyBox%" equ "" set _IncludeBusyBox=no
+
 call :GetBatchFileDirectory _MyDir
 
 set VCPKG_ROOT=
@@ -12,10 +15,10 @@ set JAVA_HOME=%ProgramW6432%\Eclipse Adoptium\jdk-24.0.1.9-hotspot
 set JDK_HOME=%ProgramW6432%\Eclipse Adoptium\jdk-24.0.1.9-hotspot
 set VCPKG_ROOT=%_MyDir%
 set CMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake
-set C_INCLUDE_PATH=%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\14.2.0\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include\sec_api;%SystemDrive%\mingw64\include;%SystemDrive%\mingw64\opt\include;%VCPKG_ROOT%\installed\x64-mingw-dynamic\include
-set CPLUS_INCLUDE_PATH=%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\14.2.0\include;%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\14.2.0\include\c++;%SystemDrive%\mingw64\x86_64-w64-mingw32\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include\sec_api;%SystemDrive%\mingw64\include;%SystemDrive%\mingw64\opt\include;%VCPKG_ROOT%\installed\x64-mingw-dynamic\include
-set INCLUDE=%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\14.2.0\include;%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\14.2.0\include\c++;%SystemDrive%\mingw64\x86_64-w64-mingw32\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include\sec_api;%SystemDrive%\mingw64\include;%SystemDrive%\mingw64\opt\include;%VCPKG_ROOT%\installed\x64-mingw-dynamic\include
-set LIBRARY_PATH=%SystemDrive%\mingw64\lib;%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\14.2.0;%SystemDrive%\mingw64\opt\lib;%SystemDrive%\mingw64\x86_64-w64-mingw32\lib;%VCPKG_ROOT%\installed\x64-mingw-dynamic\lib
+set C_INCLUDE_PATH=%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\15.1.0\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include\sec_api;%SystemDrive%\mingw64\include;%SystemDrive%\mingw64\opt\include;%VCPKG_ROOT%\installed\x64-mingw-dynamic\include
+set CPLUS_INCLUDE_PATH=%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\15.1.0\include;%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\15.1.0\include\c++;%SystemDrive%\mingw64\x86_64-w64-mingw32\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include\sec_api;%SystemDrive%\mingw64\include;%SystemDrive%\mingw64\opt\include;%VCPKG_ROOT%\installed\x64-mingw-dynamic\include
+set INCLUDE=%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\15.1.0\include;%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\15.1.0\include\c++;%SystemDrive%\mingw64\x86_64-w64-mingw32\include;%SystemDrive%\mingw64\x86_64-w64-mingw32\include\sec_api;%SystemDrive%\mingw64\include;%SystemDrive%\mingw64\opt\include;%VCPKG_ROOT%\installed\x64-mingw-dynamic\include
+set LIBRARY_PATH=%SystemDrive%\mingw64\lib;%SystemDrive%\mingw64\lib\gcc\x86_64-w64-mingw32\15.1.0;%SystemDrive%\mingw64\opt\lib;%SystemDrive%\mingw64\x86_64-w64-mingw32\lib;%VCPKG_ROOT%\installed\x64-mingw-dynamic\lib
 set VCPKG_DEFAULT_HOST_TRIPLET=x64-mingw-dynamic
 set VCPKG_DEFAULT_TRIPLET=x64-mingw-dynamic
 
@@ -48,7 +51,7 @@ for %%a in (
 "%_MyDir%\installed\x64-mingw-dynamic\bin"
 "%SystemDrive%\mingw64\x86_64-w64-mingw32\bin"
 "%SystemDrive%\mingw64\opt\bin"
-"%SystemDrive%\mingw64\libexec\gcc\x86_64-w64-mingw32\14.2.0"
+"%SystemDrive%\mingw64\libexec\gcc\x86_64-w64-mingw32\15.1.0"
 "%SystemDrive%\mingw64\bin"
 ) do (
   call :PrependToPathIfExists "%%~a"
@@ -89,8 +92,16 @@ goto :EOF
   if exist "%~1\." set PATH=%~1;%PATH%
 goto :EOF
 
-
 :ShowDebugMessage
   if "%_ShowDebugMessages%" neq "yes" exit /b 1
   echo %~1
+goto :EOF
+
+:AddBusyBox
+  if "%_IncludeBusyBox%" neq "yes" exit /b 1
+  for %%a in (
+  "%SystemDrive%\mingw64\opt\BusyBox"
+  ) do (
+    call :AppendToPathIfExists "%%~a"
+  )
 goto :EOF
