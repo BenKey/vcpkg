e09d1aacd305c75f4a4c2935f3c8a9a712f2959c
Benilda Key <bkey76@gmail.com>
2025-06-20 00:45:00 -0500

    Updates for GNU/Linux.

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+vcpkgRootDir=$(X= cd -- "$(dirname -- "$0")" && pwd -P)
+declare -a subdirectories
+subdirectories=( "bt" "buildtrees" "downloads" "installed" "packages" )
+
+for dir in "${subdirectories[@]}"
+do
+  if [ -d "${vcpkgRootDir}/${dir}" ]
+  then
+    echo "Deleting directory: $dir"
+    rm -rf "${vcpkgRootDir}/${dir}"
+  else
+    echo "Directory not found: ${vcpkgRootDir}/${dir}"
+  fi
+done
+
