ffda2c4919de97a6dc8eb455619b67e949d7446f
Benilda Key <ben.key@yekneb.com>
2026-02-05 10:29:03 -0600

    Updated vcpkg-env.el.

--- vcpkg-env.el
+++ vcpkg-env.el
@@ -4,7 +4,28 @@
 (require 'yekneb-env)
 (require 'yekneb-path-manip)
 
-(defconst vcpkg--add-to-path-windows
+(defconst vcpkg--add-to-path-x64-linux
+  '(
+    "${VCPKG_ROOT}/downloads/tools/cmake-3.30.1-linux/cmake-3.30.1-linux-x86_64/bin"
+    "${VCPKG_ROOT}/downloads/tools/ninja/1.10.2-linux"
+    "${VCPKG_ROOT}/installed/x64-linux/tools"
+    )
+  "Directories to add to the path for x64-linux."
+  )
+
+(defconst vcpkg--add-to-path-x64-mingw-dynamic
+  '(
+    "${VCPKG_ROOT}/downloads/tools/7zip-25.01-windows"
+    "${VCPKG_ROOT}/downloads/tools/7zr-25.01-windows"
+    "${VCPKG_ROOT}/downloads/tools/meson-1.9.0-d1fcc2"
+    "${VCPKG_ROOT}/downloads/tools/ninja-1.13.2-windows"
+    "${VCPKG_ROOT}/downloads/tools/python/python-3.14.2-x64-1"
+    "${VCPKG_ROOT}/installed/x64-mingw-dynamic/tools"
+    )
+  "Directories to add to the path for x64-mingw-dynamic."
+  )
+
+(defconst vcpkg--add-to-path-x64-windows
   '(
     "${VCPKG_ROOT}/downloads/tools/cmake-3.30.1-windows/cmake-3.30.1-windows-i386/bin"
     "${VCPKG_ROOT}/downloads/tools/jom/jom-1_1_4"
@@ -13,33 +34,60 @@
     "${VCPKG_ROOT}/downloads/tools/perl/5.42.0.1/c/x86_64-w64-mingw32/bin"
     "${VCPKG_ROOT}/downloads/tools/perl/5.42.0.1/perl/site/bin"
     "${VCPKG_ROOT}/downloads/tools/perl/5.42.0.1/perl/bin"
-    "${VCPKG_ROOT}/downloads/tools/python/python-3.12.7-${Platform}"
-    "${VCPKG_ROOT}/downloads/tools/python/python-3.12.7-${Platform}-1"
+    "${VCPKG_ROOT}/downloads/tools/python/python-3.12.7-x64"
+    "${VCPKG_ROOT}/downloads/tools/python/python-3.12.7-x64-1"
     "${VCPKG_ROOT}/downloads/tools/win_bison/2.5.24"
-    "${VCPKG_ROOT}/installed/${Platform}-windows/bin"
-    "${VCPKG_ROOT}/installed/${Platform}-windows/tools"
+    "${VCPKG_ROOT}/installed/x64-windows/bin"
+    "${VCPKG_ROOT}/installed/x64-windows/tools"
    )
-  "Directories to add to the path for Windows."
+  "Directories to add to the path for x64-windows."
  )
 
-(defconst vcpkg--add-to-path-linux
-  '(
-    "${VCPKG_ROOT}/downloads/tools/cmake-3.30.1-linux/cmake-3.30.1-linux-x86_64/bin"
-    "${VCPKG_ROOT}/downloads/tools/ninja/1.10.2-linux"
-    "${VCPKG_ROOT}/installed/${Platform}-linux/tools"
+(defun add-tools-directories-to-path (tools-dir)
+  (yekneb-log yekneb-log-info "tools-dir is '%s'." tools-dir)
+  (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
+  (when (file-directory-p tools-dir)
+    (setq tool-dirs (directory-files tools-dir))
+    (dolist (tool-dir tool-dirs)
+      (when (and (not (string= tool-dir ".")) (not (string= tool-dir "..")))
+        (setq tool-dir (expand-file-name tool-dir tools-dir))
+        (when (file-directory-p tool-dir)
+          (yekneb-add-to-path tool-dir t)
+          )
+        (setq tool-dir (expand-file-name "bin" tool-dir))
+        (when (file-directory-p tool-dir)
+          (yekneb-add-to-path tool-dir t)
+          )
+        )
+      )
     )
-  "Directories to add to the path for Linux."
   )
 
-(defun update-environment-for-vcpkg-windows ()
-  "Updates the environment for VCPKG on Windows."
+(defun update-environment-for-vcpkg-x64-linux ()
+  "Updates the environment for VCPKG for x64-linux."
   (let
     (
      (yekneb-debug-level (if init-file-debug yekneb-log-debug yekneb-log-entry))
      (load-file-dir (file-name-directory load-file-name))
-     (platform nil)
      (tool-dirs nil)
-     (tools-dir "${VCPKG_ROOT}/installed/${Platform}-windows/tools")
+     (tools-dir "${VCPKG_ROOT}/installed/x64-linux/tools")
+     )
+    (setq tools-dir (substitute-env-vars tools-dir))
+    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-x64-linux to the path.")
+    (yekneb-add-dirs-to-path vcpkg--add-to-path-x64-linux t t load-file-dir)
+    (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
+    (add-tools-directories-to-path tools-dir)
+    )
+  )
+
+(defun update-environment-for-vcpkg-x64-mingw-dynamic ()
+  "Updates the environment for VCPKG for x64-mingw-dynamic."
+  (let
+    (
+     (yekneb-debug-level (if init-file-debug yekneb-log-debug yekneb-log-entry))
+     (load-file-dir (file-name-directory load-file-name))
+     (tool-dirs nil)
+     (tools-dir "${VCPKG_ROOT}/installed/x64-mingw-dynamic/tools")
      (poedit-dirs
       '(
         "${ProgramFiles(x86)}/Poedit"
@@ -47,105 +95,51 @@
         )
       )
      )
-    (setq
-     platform
-     (cond
-      (yekneb-32-bit-system-p
-       "x86"
-       )
-      (yekneb-64-bit-system-p
-       "x64"
-       )
-      (t
-       nil)
-      )
-     )
-    (when platform
-      (yekneb-log yekneb-log-info "Setting Platform to '%s'." platform)
-      (yekneb-setenv "Platform" platform)
-      )
     (setq tools-dir (substitute-env-vars tools-dir))
-    (yekneb-log yekneb-log-info "tools-dir is '%s'." tools-dir)
-    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-windows to the path.")
-    (yekneb-add-dirs-to-path vcpkg--add-to-path-windows t t load-file-dir)
-    (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
-    (when (file-directory-p tools-dir)
-      (setq tool-dirs (directory-files tools-dir))
-      (dolist (tool-dir tool-dirs)
-        (when (and (not (string= tool-dir ".")) (not (string= tool-dir "..")))
-          (setq tool-dir (expand-file-name tool-dir tools-dir))
-          (when (file-directory-p tool-dir)
-            (yekneb-add-to-path tool-dir t)
-            )
-          (setq tool-dir (expand-file-name "bin" tool-dir))
-          (when (file-directory-p tool-dir)
-            (yekneb-add-to-path tool-dir t)
-            )
-          )
-        )
-      )
+    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-x64-mingw-dynamic to the path.")
+    (yekneb-add-dirs-to-path vcpkg--add-to-path-x64-mingw-dynamic t t load-file-dir)
+    (add-tools-directories-to-path tools-dir)
     (yekneb-log yekneb-log-info "Adding the directories in poedit-dirs to the path.")
     (yekneb-add-dirs-to-path poedit-dirs t t load-file-dir)
     )
   )
 
-(defun update-environment-for-vcpkg-linux ()
-  "Updates the environment for VCPKG on Linux."
+(defun update-environment-for-vcpkg-x64-windows ()
+  "Updates the environment for VCPKG for x64-windows."
   (let
     (
      (yekneb-debug-level (if init-file-debug yekneb-log-debug yekneb-log-entry))
      (load-file-dir (file-name-directory load-file-name))
-     (platform nil)
      (tool-dirs nil)
-     (tools-dir "${VCPKG_ROOT}/installed/${Platform}-linux/tools")
-     )
-    (setq
-     platform
-     (cond
-      (yekneb-32-bit-system-p
-       "x86"
-       )
-      (yekneb-64-bit-system-p
-       "x64"
-       )
-      (t
-       nil)
-      )
-     )
-    (when platform
-      (yekneb-log yekneb-log-info "Setting Platform to '%s'." platform)
-      (yekneb-setenv "Platform" platform)
-      )
-    (setq tools-dir (substitute-env-vars tools-dir))
-    (yekneb-log yekneb-log-info "tools-dir is '%s'." tools-dir)
-    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-linux to the path.")
-    (yekneb-add-dirs-to-path vcpkg--add-to-path-linux t t load-file-dir)
-    (yekneb-log yekneb-log-info "Adding the directories in tool-dirs to the path.")
-    (when (file-directory-p tools-dir)
-      (setq tool-dirs (directory-files tools-dir))
-      (dolist (tool-dir tool-dirs)
-        (when (and (not (string= tool-dir ".")) (not (string= tool-dir "..")))
-          (setq tool-dir (expand-file-name tool-dir tools-dir))
-          (when (file-directory-p tool-dir)
-            (yekneb-add-to-path tool-dir t)
-            )
-          (setq tool-dir (expand-file-name "bin" tool-dir))
-          (when (file-directory-p tool-dir)
-            (yekneb-add-to-path tool-dir t)
-            )
-          )
+     (tools-dir "${VCPKG_ROOT}/installed/x64-windows/tools")
+     (poedit-dirs
+      '(
+        "${ProgramFiles(x86)}/Poedit"
+        "${ProgramFiles(x86)}/Poedit/GettextTools/bin"
         )
       )
+     )
+    (setq tools-dir (substitute-env-vars tools-dir))
+    (yekneb-log yekneb-log-info "Adding the directories in vcpkg--add-to-path-x64-windows to the path.")
+    (yekneb-add-dirs-to-path vcpkg--add-to-path-x64-windows t t load-file-dir)
+    (add-tools-directories-to-path tools-dir)
+    (yekneb-log yekneb-log-info "Adding the directories in poedit-dirs to the path.")
+    (yekneb-add-dirs-to-path poedit-dirs t t load-file-dir)
     )
   )
 
 (defun update-environment-for-vcpkg ()
   "Updates the environment for VCPKG."
   (when (eq system-type 'windows-nt)
-    (update-environment-for-vcpkg-windows)
+    (when (file-directory-p (substitute-env-vars "${VCPKG_ROOT}/installed/x64-mingw-dynamic"))
+      (update-environment-for-vcpkg-x64-mingw-dynamic)
+      )
+    (when (file-directory-p (substitute-env-vars "${VCPKG_ROOT}/installed/x64-windows"))
+      (update-environment-for-vcpkg-x64-windows)
+      )
     )
   (when (eq system-type 'gnu/linux)
-    (update-environment-for-vcpkg-linux)
+    (update-environment-for-vcpkg-x64-linux)
     )
   )
 
