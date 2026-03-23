ac7604813892a8002a318b3a1857c37daa5bbf19
Benilda Key <bkey76@gmail.com>
2026-03-23 00:05:50 -0500

    Fix errors related to endian.h not existing on all platforms. This patch replaces the earlier Apple specific patch.

--- ports/hash-library/portfile.cmake
+++ ports/hash-library/portfile.cmake
@@ -9,7 +9,7 @@ vcpkg_from_github(
     SHA512 1c428710c0c3e4d5d1114d757a5d9145ed12c7e2fbbfa51635f43a349ddb5634bdf49e8d8fdbc7576e90b319989fb85efec433bb43ddb551c2cf29a8e80ba78b
     HEAD_REF master
     PATCHES
-        001-fix-macos.patch
+        001-portable-endian.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
