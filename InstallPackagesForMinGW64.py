5765b0a24b57bf5baed50dff5f7852d776621717
Benilda Key <bkey76@gmail.com>
2026-04-10 06:48:30 -0500

    * Fixed a error in the qtapplicationmanager port. The windres command line was too long causing the build to fail due to a command-line too long error when using MinGW.
    * Updated the GetScriptFile function so that it uses 'os.path.abspath' instead of 'os.path.realpath' so that it does not resolve symbolic links or subst drives.
    
    * Modified the GetScriptDirectory function so that it returns just the drive letter followed by : if the GetScriptFile function returns a {DriveLetter}:/ path.
    
    * Modified the script to print the Script directory.

--- InstallPackagesForMinGW64.py
+++ InstallPackagesForMinGW64.py
@@ -267,34 +267,36 @@ packageList: str_list = [
 ]
 
 def GetScriptFile() -> str:
-  """Obtains the full path and file name of the Python script."""
-  if (hasattr(GetScriptFile, "file")):
-    return getattr(GetScriptFile, "file")
-  ret: str = ""
-  try:
-    # The easy way. Just use __file__.
-    # Unfortunately, __file__ is not available when cx_freeze is used or in IDLE.
-    ret = os.path.realpath(__file__)
-  except NameError:
-    # The hard way.
-    if (len(sys.argv) > 0 and len(sys.argv[0]) > 0 and os.path.isabs(sys.argv[0])):
-      ret = os.path.realpath(sys.argv[0])
-    else:
-      ret = os.path.realpath(inspect.getfile(GetScriptFile))
-      if (not os.path.exists(ret)):
-        # If cx_freeze is used the value of the ret variable at this point is in
-        # the following format: {PathToExeFile}\{NameOfPythonSourceFile}. This
-        # makes it necessary to strip off the file name to get the correct path.
+    """Obtains the full path and file name of the Python script."""
+    if (hasattr(GetScriptFile, "file")):
+        return getattr(GetScriptFile, "file")
+    
+    ret: str = ""
+    try:
+        # Use abspath instead of realpath to keep subst drives
+        ret = os.path.abspath(__file__)
+    except NameError:
+        if (len(sys.argv) > 0 and len(sys.argv[0]) > 0 and os.path.isabs(sys.argv[0])):
+            ret = os.path.abspath(sys.argv[0])
+        else:
+            ret = os.path.abspath(inspect.getfile(GetScriptFile))
+            
+    if (not os.path.exists(ret)):
         ret = os.path.dirname(ret)
-  setattr(GetScriptFile, "file", ret)
-  return getattr(GetScriptFile, "file")
+
+    ret = os.path.normpath(ret) 
+    ret = ret.replace("\\", "/")
+    setattr(GetScriptFile, "file", ret)
+    return getattr(GetScriptFile, "file")
 
 def GetScriptDirectory() -> str:
   """Obtains the path to the directory containing the script."""
   if (hasattr(GetScriptDirectory, "dir")):
     return getattr(GetScriptDirectory, "dir")
-  module_path: str = GetScriptFile()
-  setattr(GetScriptDirectory, "dir", os.path.dirname(module_path))
+  ret: str = os.path.dirname(GetScriptFile())
+  if (len(ret) == 3 and ret[1] == ":" and ret[2] == "/"):
+    ret = ret[0:2]
+  setattr(GetScriptDirectory, "dir", ret)
   return getattr(GetScriptDirectory, "dir")
 
 def filter_list(list: str_list, excluded_list: str_list) -> str_list:
@@ -377,4 +379,8 @@ def InstallPackagesInPackageList() -> bool:
     return False
   return True
 
+scriptDirectory: str = GetScriptDirectory()
+print()
+print(f"Script directory: {scriptDirectory}")
+print()
 InstallPackagesInPackageList()
