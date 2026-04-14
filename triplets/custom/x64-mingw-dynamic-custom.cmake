69357c07b0d8fc64ae11481fe43108db750828b2
Benilda Key <bkey76@gmail.com>
2026-04-14 10:04:15 -0500

    Updated the x64-mingw-dynamic-custom triplet to ensure that the correct python is used in my MinGW64 environment when the MSYS2\usr\bin directory is in the PATH.

--- triplets/custom/x64-mingw-dynamic-custom.cmake
+++ triplets/custom/x64-mingw-dynamic-custom.cmake
@@ -3,6 +3,12 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_ENV_PASSTHROUGH PATH)
 
+find_program(VCPKG_PREFERRED_PYTHON3 NAMES python3 python)
+if(NOT VCPKG_PREFERRED_PYTHON3)
+  message(FATAL_ERROR "Python was not found on PATH. Add your preferred Python 3 location to PATH before invoking vcpkg.")
+endif()
+set(PYTHON3 "${VCPKG_PREFERRED_PYTHON3}" CACHE FILEPATH "Python used by vcpkg Meson ports" FORCE)
+
 set(VCPKG_CMAKE_SYSTEM_NAME MinGW)
 set(VCPKG_CMAKE_SYSTEM_VERSION 10)
 set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
